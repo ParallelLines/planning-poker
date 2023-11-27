9faaf64e8a921cb99612c83a3e372229b761a0c8
ParallelLines <tishkane@yandex.ru>
2023-11-27 15:06:54 +0100

    shameful error correction

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import PockerApp from './PackerApp'
+import PockerApp from './PockerApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
