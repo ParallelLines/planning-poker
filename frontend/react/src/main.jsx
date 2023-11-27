6eb5efffd10700f05305c3311742d8db208ee3ec
ParallelLines <tishkane@yandex.ru>
2023-11-27 14:57:14 +0100

    added a folder for routes

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Root from './root'
+import Root from './routes/root'
+import { createBrowserRouter } from 'react-router-dom'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
