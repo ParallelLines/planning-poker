b74c020fc4158ff937a593d6541c429349923edf
ParallelLines <tishkane@yandex.ru>
2024-01-01 18:26:24 +0100

    preparing Participants for Collapsible

--- frontend/react/src/components/Participants.jsx
+++ frontend/react/src/components/Participants.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import IconParticipants from "./IconParticipants"
+import IconCross from "./IconCross"
 
 const participantsList = [
     {
@@ -168,21 +170,17 @@ export default function Participants() {
             <div className="title">
                 <div className="progressbar"></div>
                 <button id="show-participants-btn" className="svg-btn toggle-btn" name="toggle participants" data-target="collapsible-participants-list">
-                    <svg className="switch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
-                        <path d="M18,12L10,12L10,16L14,16L10,16L10,20L12,20L12,28L16,28L16,20L18,20L18,12ZM0,20L2,20L2,28L6,28L6,20L8,20L8,12L0,12L0,20ZM26,24L22,24L22,28L26,28L26,24ZM28,20L28,12L20,12L20,20L22,20L22,24L26,24L26,20L28,20ZM6,6L2,6L2,10L6,10L6,6ZM16,6L12,6L12,10L16,10L16,6ZM26,6L22,6L22,10L26,10L26,6ZM12,-0L8,-0L8,4L12,4L12,-0ZM20,-0L16,-0L16,4L20,4L20,-0Z" />
-                    </svg>
-                    <svg className="cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
-                        <path d="M2,12L0,12L0,14L2,14L2,12ZM14,12L12,12L12,14L14,14L14,12ZM4,10L2,10L2,12L4,12L4,10ZM12,10L10,10L10,12L12,12L12,10ZM6,8L4,8L4,10L6,10L6,8ZM10,8L8,8L8,10L10,10L10,8ZM8,6L6,6L6,8L8,8L8,6ZM6,4L4,4L4,6L6,6L6,4ZM10,4L8,4L8,6L10,6L10,4ZM2,2L2,0L0,0L0,2L2,2L2,4L4,4L4,2L2,2ZM12,2L10,2L10,4L12,4L12,2ZM14,-0L12,-0L12,2L14,2L14,-0Z" />
-                    </svg>
+                    <IconParticipants />
+                    <IconCross />
                 </button>
                 <div className="vote-counter">
                     <span className="did-vote">7</span>/26
                 </div>
             </div>
-            <div id="collapsible-participants-list" className={`scrollable-col ${isMobile ? 'collapsible' : ''}`} data-toggler="show-participants-btn">
+            <ul id="collapsible-participants-list" className={`scrollable-col ${isMobile ? 'collapsible' : ''}`} data-toggler="show-participants-btn">
                 {participantsList.map((person, i) => {
                     return (
-                        <div className="participant" key={i}>
+                        <li className="participant" key={i}>
                             <span className={person.voted ? "vote-indicator voted" : "vote-indicator"}></span>
                             <span className="participant-name">{person.name}
                                 {person.isAdmin ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 26" width="15" hight="15">
@@ -190,10 +188,10 @@ export default function Participants() {
                                 </svg> : null}
                             </span>
                             <span className="result">{person.result}</span>
-                        </div>
+                        </li>
                     )
                 })}
-            </div>
+            </ul>
         </aside>
     )
 }
