255df81cef1c4b852c2f0e5fda5c78f9963d6408
ParallelLines <tishkane@yandex.ru>
2024-01-22 17:27:53 +0100

    copy link works (but need to think how to make it responsive)

--- frontend/react/src/components/CopyLinkButton.jsx
+++ frontend/react/src/components/CopyLinkButton.jsx
@@ -1,9 +1,22 @@
-import IconCopy from "./IconCopy";
+// import { useRef } from 'react'
+import IconCopy from './IconCopy'
+// import gsap from 'gsap'
 
 export default function CopyLinkButton({ sessionId }) {
+    // const copiedMsg = useRef(null)
+
+    const copyLinkToClip = async () => {
+        await navigator.clipboard.writeText(location.href)
+        // gsap.timeline()
+        //     .to(copiedMsg.current, {opacity: 1, duration: 0.01})
+        //     .to(copiedMsg.current, {opacity: 0, scale: 2, duration: 1})
+        console.log('link copied! :)')
+    }
+
     return (
         <div className="copy-link">
-            <button id="copy-link-btn" className="svg-btn" name="copy link">
+            {/* <span ref={copiedMsg}>copied!</span> */}
+            <button id="copy-link-btn" className="svg-btn" name="copy link" onClick={copyLinkToClip}>
                 <IconCopy />
             </button>
             <label htmlFor="copy-link-btn">session id {sessionId}</label>
