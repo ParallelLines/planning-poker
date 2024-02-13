a20ece3e0f586fa8b56ab3463b5acd3889e16669
ParallelLines <tishkane@yandex.ru>
2024-02-13 19:48:14 +0100

    deleted error boundary

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import PockerApp from './PockerApp'
 import { BrowserRouter } from 'react-router-dom'
-import { ErrorBoundary } from 'react-error-boundary'
-import ErrorPage from './components/ErrorPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ErrorBoundary fallback={<ErrorPage message={'something reeeeeally went wrong :('} />}>
-      <PockerApp />
-    </ErrorBoundary>
+    <PockerApp />
   </BrowserRouter>,
 )
