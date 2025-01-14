ec8a99fda53345d1173e045eea5c6e71cc3031db
ParallelLines <tishkane@yandex.ru>
2025-01-14 16:12:59 +0100

    front fix: added a human icon so it's clear that in brackets is a number of participants who voted

--- frontend/react/src/components/Result.jsx
+++ frontend/react/src/components/Result.jsx
@@ -26,7 +26,7 @@ export default function Result({ votes }) {
         <div className="pi-chart">
             <PieChart
                 data={data}
-                label={data => data.dataEntry.title + ' (' + data.dataEntry.value + ')'}
+                label={data => data.dataEntry.title + ' (👤' + data.dataEntry.value + ')'}
                 labelPosition={70}
                 animate
                 animationDuration={400}
